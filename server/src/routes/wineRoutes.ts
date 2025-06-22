157bc0aefda49052e0a2d05457893bc5ed19f7e2
Alastair Lloyd <dex8056@aol.com>
2025-06-22 08:30:08 +0100

    chore: scaffold route and controller boilerplate for users, wines, ratings, and favorites

--- server/src/routes/wineRoutes.ts
+++ server/src/routes/wineRoutes.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import {
+  getAllWines,
+  getWineById,
+  getFilteredWines,
+  getSurpriseWine
+} from '../controllers/wineController';
 
-import { getWines } from '../controllers/wineController'
+const router: Router = Router();
 
-const router = Router();
 
-router.post('/', getWines)
+router.get('/', getAllWines);
+router.get('/search/filter', getFilteredWines); //This being the 3 that show on the results page, RESTFUL route
+router.get('/surprise', getSurpriseWine);
+router.get('/:id', getWineById); //Not sure if we need this
 
 export default router;

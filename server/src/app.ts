9ff1cd17612ff57210c7841969cf6614f9974eea
Tom AF <tom.effraf@gmail.com>
2025-06-23 17:43:14 +0100

    step: starting search logic

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,13 +3,15 @@ import cors from 'cors';
 
 import wineRoutes from './routes/wineRoutes';
 import favouriteRoutes from './routes/favouriteRoutes';
+import searchRoutes from './routes/searchRoutes';
 
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/wines', wineRoutes);
-// app.use('/api/favourites', favouriteRoutes);
+app.use('/wines', wineRoutes);
+// app.use('/favourites', favouriteRoutes);
+app.use('/', searchRoutes);
 
 export default app;

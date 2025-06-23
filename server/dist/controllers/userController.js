ea462f771db9361b0cb9a0372969ae1c641c1048
Oli Stogdale <oli.stogdale@gmail.com>
2025-06-23 21:29:04 +0100

    patch: added blind endpoint to favourites controller to prevent errors.

---
+++ server/dist/controllers/userController.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deleteUser = exports.updateUser = exports.getUserById = void 0;
+const getUserById = async (req, res) => {
+    try {
+    }
+    catch (error) {
+    }
+};
+exports.getUserById = getUserById;
+const updateUser = async (req, res) => {
+    try {
+    }
+    catch (error) {
+    }
+};
+exports.updateUser = updateUser;
+const deleteUser = async (req, res) => {
+    try {
+    }
+    catch (error) {
+    }
+};
+exports.deleteUser = deleteUser;
---
+++ server/src/controllers/authController.ts
@@ -0,0 +1,12 @@
+const registerUser = (): string => {
+  const user = 'Jill'
+  return user;
+}
+
+const loginUser = (): string => {
+  const user = 'Bob'
+  return user;
+}
+
+
+export { registerUser, loginUser }

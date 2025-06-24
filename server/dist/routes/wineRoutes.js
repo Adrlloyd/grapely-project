fa9f6bb0950a058fd30bcfd8b3536189c67b3c17
Oli Stogdale <oli.stogdale@gmail.com>
2025-06-24 12:38:11 +0100

    fix: saving local scraper & dependency settings

--- server/dist/routes/wineRoutes.js
+++ server/dist/routes/wineRoutes.js
@@ -4,7 +4,7 @@ const express_1 = require("express");
 const wineController_1 = require("../controllers/wineController");
 const router = (0, express_1.Router)();
 router.get('/', wineController_1.getAllWines);
-router.get('/search/filter', wineController_1.getRecommendedWines); //This being the 3 that show on the results page, RESTFUL route
+router.post('/search/filter', wineController_1.getRecommendedWines); //This being the 3 that show on the results page, RESTFUL route
 router.get('/surprise', wineController_1.getSurpriseWine);
 router.get('/:id', wineController_1.getWineById); //Not sure if we need this
 exports.default = router;

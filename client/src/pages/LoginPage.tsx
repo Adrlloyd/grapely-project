7bf6744f77ac04a6a79da7f198995f5a39ccc8a1
Yann Adem <yann.adem@gmail.com>
2025-06-26 14:45:06 +0100

    Feat(auth): Created auth context, Pages: Login,Register, Components: LoginForm

---
+++ client/src/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import LoginForm from '../components/LoginForm';
+
+const LoginPage = () => {
+
+  return (
+    <>
+      <h2>Create an account</h2>
+      <LoginForm />
+    </>
+  );
+}
+
+export default LoginPage;

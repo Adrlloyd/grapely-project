330f7a2516b24e0b8d980822e39023db223d3a6f
Oli Stogdale <oli.stogdale@gmail.com>
2025-06-26 15:46:12 +0100

    stash: working changes saved whilst pulling new v of dev

---
+++ client/src/components/UserProfile.tsx
@@ -0,0 +1,24 @@
+// import { useState, useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import '../styles/UserProfile.css';
+// // import modal component?
+// import { registerUser, loginUser} from '../services/userService';
+
+function UserProfile() {
+  // const navigate = useNavigate(); //To be used for the 'deleteAccount' function - also set {replace:true} as argument to disable return nav through back button
+  // const token: string = localStorage.getItem('token');
+
+
+  // const [name, setName] = useState<string>('');
+  // const [username, setUsername] = useState<string>('');
+  // const [currentPassword, setCurrentPassword] = useState<string>('');
+  // const [newPassword, setNewPassword] = useState<string>('');
+  // const [status, setStatus] = useState<string>('');
+
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default UserProfile;
---
+++ client/src/pages/UserProfilePage.tsx
@@ -0,0 +1,11 @@
+import UserProfile from '../components/Selection';
+
+function UserProfilePage() {
+  return (
+    <div>
+      <UserProfile />
+    </div>
+  );
+}
+
+export default UserProfilePage;
